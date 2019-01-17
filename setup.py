455bbb64eb4aea052cefac00946926b4a320caf3
Carlos <nzlosh@yahoo.com>
2019-01-17 23:26:59 +0100

    Add setup.py for use with pip install.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    author_email="nobody@localhost",
+    author="err-backend-mattermost contributors",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    description="A Mattermost backend for Errbot.",
+    keywords='errbot mattermost',
+    long_description_content_type="text/markdown",
+    long_description="Not available",
+    name="err-backend-mattermost",
+    packages=setuptools.find_packages(),
+    url="https://github.com/nzlosh/err-backend-mattermost",
+    version="2.0.2",
+)
